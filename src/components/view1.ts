08e802c40d5d2bd287b80c8630bfbd46859eb8c0
t-fukamizu <takuro.f.1987@gmail.com>
2017-12-17 17:50:05 +0900

    fix line end.

--- src/components/view1.ts
+++ src/components/view1.ts
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import Vue from 'vue';
+import Component from 'vue-class-component';
 
 @Component({
-    template: require('./view1.html')
+    template: require('./view1.html');
 })
 
 export class View1 extends Vue {
     // dataメンバー =======================
-    message: string = "Hello Vue!"
+    message: string = "Hello Vue!";
 
     // methodsメンバー =======================
     reverseMessage() : void {
-        this.message = this.message.split('').reverse().join('')
+        this.message = this.message.split('').reverse().join('');
     }
 }
